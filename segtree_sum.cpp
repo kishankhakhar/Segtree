bf1443bbe0977865ecc17cb582fa21f1719e63cf
Rohit Choudhary <rc67@outlook.com>
2016-04-27 18:59:11 +0530

    Added files via upload

---
+++ segtree_sum.cpp
@@ -0,0 +1,108 @@
+//Sum-----segtree
+#include <conio.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+#include<string.h>
+#include<math.h>
+
+#define N 8
+#define MAX (1+(1<<6))
+#define inf 0x7fffffff
+
+int arr[N];
+int tree[MAX];
+int lazy[MAX];
+
+/**
+ * Build and init tree
+ */
+void build_tree(int node, int a, int b) {
+  	if(a > b) return; // Out of range
+
+  	if(a == b) { // Leaf node
+    		tree[node] = arr[a]; // Init value
+		return;
+	}
+
+	build_tree(node*2, a, (a+b)/2); // Init left child
+	build_tree(node*2+1, 1+(a+b)/2, b); // Init right child
+
+	tree[node] = tree[node*2]+tree[node*2+1]; // Init root value
+}
+
+/*
+ * Increment elements within range [i, j] with value value
+ */
+void update_tree(int node, int a, int b, int i, int j, int value) {
+
+  	if(lazy[node] != 0) { // This node needs to be updated
+   		tree[node] += lazy[node]; // Update it
+
+		if(a != b) {
+			lazy[node*2] += lazy[node]; // Mark child as lazy
+    		lazy[node*2+1] += lazy[node]; // Mark child as lazy
+		}
+
+   		lazy[node] = 0; // Reset it
+  	}
+
+	if(a > b || a > j || b < i) // Current segment is not within range [i, j]
+		return;
+
+  	if(a >= i && b <= j) { // Segment is fully within range
+    		tree[node] += value;
+
+		if(a != b) { // Not leaf node
+			lazy[node*2] += value;
+			lazy[node*2+1] += value;
+		}
+    	return;
+	}
+
+	update_tree(node*2, a, (a+b)/2, i, j, value); // Updating left child
+	update_tree(1+node*2, 1+(a+b)/2, b, i, j, value); // Updating right child
+
+	tree[node] = tree[node*2] + tree[node*2+1]; // Updating root with sum of child
+}
+
+int queryRange(int node, int start, int end, int l, int r)
+{
+    if(start > end or start > r or end < l)
+        return 0;         // Out of range
+    if(lazy[node] != 0)
+    {
+        // This node needs to be updated
+        tree[node] += (end - start + 1) * lazy[node];            // Update it
+        if(start != end)
+        {
+            lazy[node*2] += lazy[node];         // Mark child as lazy
+            lazy[node*2+1] += lazy[node];    // Mark child as lazy
+        }
+        lazy[node] = 0;                 // Reset it
+    }
+    if(start >= l and end <= r)             // Current segment is totally within range [l, r]
+        return tree[node];
+    int mid = (start + end) / 2;
+    int p1 = queryRange(node*2, start, mid, l, r);         // Query left child
+    int p2 = queryRange(node*2 + 1, mid + 1, end, l, r); // Query right child
+    return (p1 + p2);
+}
+
+int main() {
+
+	for(int i = 0; i < N; i++){
+		cin>>arr[i];
+	}
+
+	build_tree(1, 0, N-1);
+
+	memset(lazy, 0, sizeof lazy);
+
+	update_tree(1, 0, N-1, 0, 6, 5); // Increment range [0, 6] by 5. here 0, N-1 represent the current range.
+
+	cout << queryRange(1, 0, N-1, 1, 2) << endl; //
+  //getch();
+  return 0;
+}
